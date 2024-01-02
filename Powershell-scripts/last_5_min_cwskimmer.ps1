51407f88d4afd9ac3fec0bfeff4b94caa4a18759
Stefan Dambeck <35285029+dc7ds@users.noreply.github.com>
2024-01-02 09:13:16 +0100

    Update last_5_min_cwskimmer.ps1

--- Powershell-scripts/last_5_min_cwskimmer.ps1
+++ Powershell-scripts/last_5_min_cwskimmer.ps1
@@ -9,15 +9,7 @@ $Content = (Get-Content $cwskimmerlog -tail 5000).where({
 
 $Content | Out-File $outfile
 
-#$Content2 = (Get-Content C:\Users\dl0pf\AppData\Roaming\Afreet\Products\SkimSrv2\Spots.txt).where({
-#    [datetime]($_ -replace "(^\S+\s\S+).*",'$1') -ge $TargetTime
-#},'SkipUntil')
-
-#$Content2 | Out-File $outfile2
-
-
 ((Get-Content $outfile) -join "`n") + "`n" | Set-Content -NoNewline $outfile
-
 
 #Invoke-Expression -Command "c:\Scripts\Copy_cwspots_2_HUP.cmd"
 
